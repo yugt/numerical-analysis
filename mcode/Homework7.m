fb98b30f7507786714def8b1aebb7906368094d1
Guangting Yu <yugtmath@umich.edu>
2017-07-19 13:35:34 -0400

    matlab newton b for 5

--- mcode/Homework7.m
+++ mcode/Homework7.m
@@ -1,9 +1,12 @@
 %%%%	Exercie 7.3
-format long
-f = @(x) tan(x)-4*x;
-df = @(x) 1/(cos(x)^2)-4;
-newton(f,df,1.57,1e-4,1e-3,100)
-newton(f,df,4.7,1e-4,1e-3,100)
+% format long
+% f = @(x) tan(x)-4*x;
+% df = @(x) 1/(cos(x)^2)-4;
+% newton(f,df,1.57,1e-4,1e-3,100)
+% newton(f,df,4.7,1e-4,1e-3,100)
 
 
 %%%%	Exercise 7.5
+f = @(x) sin(x^3);
+df = @(x) 3*x^2*cos(x^3);
+newton_Modified(f,df,1,3,1e-10,1e-10,100)
