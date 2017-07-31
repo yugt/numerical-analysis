9a70ecc9c350ec5edb18426f017d863d67f6a6b7
Yu <yugtmath@UMICH.EDU>
2017-07-31 15:53:42 -0400

    temp

---
+++ mcode/backwardeuler.m
@@ -0,0 +1,16 @@
+function [ BE ] = backwardeuler( f,a,b,ya,M )
+
+h=(b-a)/M;
+T=zeros(1,M+1);
+Y=zeros(1,M+1);
+T=a:h:b;
+Y(1)=ya;
+
+for j=1:M
+   Y(j+1)=Y(j)+h*f(T(j),Y(j));
+end
+
+BE=[T' Y'];
+
+end
+
