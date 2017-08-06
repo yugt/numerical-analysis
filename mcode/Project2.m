b11eba877e651528e9d9cad204dca1a0d072e98e
Guest User <yu-guangting0524@sjtu.edu.cn>
2017-08-06 18:30:30 +0800

    full book m code uncommented

--- mcode/Project2.m
+++ mcode/Project2.m
@@ -1,34 +1,33 @@
 %%%%%% Task 1
 
 % Fixed-point iteration
-% g=@(x)[x(1)^2+x(1)-x(2);x(1)^2+x(2)^2+x(2)-1];
-% P=fixedpoint(g,[0.786;0.618],1e-6,100);
+g=@(x)[x(1)^2+x(1)-x(2);x(1)^2+x(2)^2+x(2)-1];
+P=fixedpoint(g,[0.786;0.618],1e-6,100);
 
 
 % Why fixed-point fail
-% Jg=[sqrt(2*(sqrt(5)-1))+1, -1; sqrt(2*(sqrt(5)-1)), sqrt(5)];
-% norm(Jg,2)
+Jg=[sqrt(2*(sqrt(5)-1))+1, -1; sqrt(2*(sqrt(5)-1)), sqrt(5)];
+norm(Jg,2)
 
 % Newton's method for higher dimension
-% f=@(x) [x(1)^2-x(2);x(1)^2+x(2)^2-1];
-% Jf=@(x) [ 2*x(1), -1; 2*x(1), 2*x(2)];
-% X1=newton_Highdim( f,Jf,[10;1],1e-300,1e-300,10);
-
-% X1=newton_Highdim( f,Jf,[-10;2],1e-300,1e-300,10);
+f=@(x) [x(1)^2-x(2);x(1)^2+x(2)^2-1];
+Jf=@(x) [ 2*x(1), -1; 2*x(1), 2*x(2)];
+X1=newton_Highdim( f,Jf,[10;1],1e-300,1e-300,10);
+X1=newton_Highdim( f,Jf,[-10;2],1e-300,1e-300,10);
 
 
 % Secant method for higher dimension
-% X=secant_Highdim(f,Jf,[0;0],[1;1],1e-300,1e-300,20);
-% X=secant_Highdim(f,Jf,[0;0],[-1;-1],1e-30,1e-30,30);
-% [abs(X(1,:)+sqrt((sqrt(5)-1)/2))' abs(X(2,:)-(sqrt(5)-1)/2)']
+X=secant_Highdim(f,Jf,[0;0],[1;1],1e-300,1e-300,20);
+X=secant_Highdim(f,Jf,[0;0],[-1;-1],1e-30,1e-30,30);
+[abs(X(1,:)+sqrt((sqrt(5)-1)/2))' abs(X(2,:)-(sqrt(5)-1)/2)']
 
 % loglog(abs(X(1,:)-sqrt((sqrt(5)-1)/2)),abs(X(2,:)-(sqrt(5)-1)/2))
 
 
 %%%%%% Task 2
-% f=@(t) 3*sec(t)-2*cos(tan(t)/1000*(1-10*pi/3-exp(-sec(t)*tan(t)/100)))-1;
-% df=@(t) 3*tan(t)*sec(t)+(sec(t)*exp(-(1/100)*tan(t)*sec(t))*(3*tan(t)^3+3*tan(t)*sec(t)^2-100*sec(t)*((10*pi-3)*exp(1/100*tan(t)*sec(t))+3))*sin((tan(t)*(-exp(-(1/100)*tan(t)*sec(t))+1-(10*pi)/3))/1000))/150000;
-% newton(f,df,1,1e-16,1e-16,10)
+f=@(t) 3*sec(t)-2*cos(tan(t)/1000*(1-10*pi/3-exp(-sec(t)*tan(t)/100)))-1;
+df=@(t) 3*tan(t)*sec(t)+(sec(t)*exp(-(1/100)*tan(t)*sec(t))*(3*tan(t)^3+3*tan(t)*sec(t)^2-100*sec(t)*((10*pi-3)*exp(1/100*tan(t)*sec(t))+3))*sin((tan(t)*(-exp(-(1/100)*tan(t)*sec(t))+1-(10*pi)/3))/1000))/150000;
+newton(f,df,1,1e-16,1e-16,10)
 % 
 % 
 % fplot(@(t) df(t),[-1,1])
@@ -77,18 +76,18 @@ E=((y'-et)'*(y'-et)); % error
 double(subs(E,{a,b,c},{1.53,2390,288}))
 temp=7e05;
 
-% cand=[];
-% for i=1.50:0.01:1.53
-%     for j=2350:2400
-%         for k=180:195
-%             if double(subs(E,{a,b,c},{i,j,k}))<temp
-%                 temp=double(subs(E,{a,b,c},{i,j,k}))
-%                 cand=[cand [i;j;k;temp]];
-%                 double(subs(g,{a,b,c},{i,j,k}))
-%             end
-%         end
-%     end
-% end
+cand=[];
+for i=1.50:0.01:1.53
+	for j=2350:2400
+		for k=180:195
+			if double(subs(E,{a,b,c},{i,j,k}))<temp
+				temp=double(subs(E,{a,b,c},{i,j,k}))
+				cand=[cand [i;j;k;temp]];
+				double(subs(g,{a,b,c},{i,j,k}))
+			end
+		end
+	end
+end
 
 dy=y-exp(2390.91074802608./(x+188.153946322168))*1.53306388436911;
 set(gca(), 'LooseInset', get(gca(), 'TightInset'));
@@ -126,60 +125,60 @@ H=hessian(E,[a,b,c]);
 %Secant method
 
 
-% x0=[1.53395060236349;2390.57619051588;188.134672703047];
-% x1=[1.53355930237206;2390.72624233791;188.143059536112];
-% 
-% X=[];
-% 
-% 
-% s=x1-x0;
-% z=double(subs(g,{a,b,c},{x1(1),x1(2),x1(3)})-subs(g,{a,b,c},{x0(1),x0(2),x0(3)}));
-% Jf=double(subs(H,{a,b,c},{x0(1),x0(2),x0(3)}));
-% A=Jf+(z-Jf*s)*s'/norm(s,2)^2;
-% w=A\z;
-% X=[X x0];
-% x0=x1;
-% x1=x1-w;
-% 
-% 
-% for k=1:100
-%     if(s'*s<1e-10)||(z'*z<1e-10)
-%         break
-%     end
-%     s=x1-x0;
-%     z=double(subs(g,{a,b,c},{x1(1),x1(2),x1(3)})-subs(g,{a,b,c},{x0(1),x0(2),x0(3)}));
-%     Jf=double(subs(H,{a,b,c},{x0(1),x0(2),x0(3)}));
-%     A=Jf+(z-Jf*s)*s'/norm(s,2)^2;
-%     w=A\double(subs(g,{a,b,c},{x1(1),x1(2),x1(3)}));
-%     X=[X x0];
-%     x0=x1;
-%     x1=x1-w;
-% end
-% 
-% X=[X x1];
+x0=[1.53395060236349;2390.57619051588;188.134672703047];
+x1=[1.53355930237206;2390.72624233791;188.143059536112];
+
+X=[];
+
+
+s=x1-x0;
+z=double(subs(g,{a,b,c},{x1(1),x1(2),x1(3)})-subs(g,{a,b,c},{x0(1),x0(2),x0(3)}));
+Jf=double(subs(H,{a,b,c},{x0(1),x0(2),x0(3)}));
+A=Jf+(z-Jf*s)*s'/norm(s,2)^2;
+w=A\z;
+X=[X x0];
+x0=x1;
+x1=x1-w;
+
+
+for k=1:100
+    if(s'*s<1e-10)||(z'*z<1e-10)
+        break
+    end
+    s=x1-x0;
+    z=double(subs(g,{a,b,c},{x1(1),x1(2),x1(3)})-subs(g,{a,b,c},{x0(1),x0(2),x0(3)}));
+    Jf=double(subs(H,{a,b,c},{x0(1),x0(2),x0(3)}));
+    A=Jf+(z-Jf*s)*s'/norm(s,2)^2;
+    w=A\double(subs(g,{a,b,c},{x1(1),x1(2),x1(3)}));
+    X=[X x0];
+    x0=x1;
+    x1=x1-w;
+end
+
+X=[X x1];
 
 
 
 
 
-% Newton Always Fail
-% p0=[1.53306388436911;2390.91074802608;188.153946322168];
-% for k=1:100
-% 	X=[X p0];
-% 	p1=p0-double(subs(H,{a,b,c},{p0(1),p0(2),p0(3)}))\double(subs(g,{a,b,c},{p0(1),p0(2),p0(3)}));
-% 	err=(p1-p0)'*(p1-p0);
-% 	p0=p1;
-%     y=double(subs(g,{a,b,c},{p0(1),p0(2),p0(3)}));
-% 	if (err<1e-16||y'*y<1e-16)
-% 		break
-% 	end
-% end
+Newton Always Fail
+p0=[1.53306388436911;2390.91074802608;188.153946322168];
+for k=1:100
+	X=[X p0];
+	p1=p0-double(subs(H,{a,b,c},{p0(1),p0(2),p0(3)}))\double(subs(g,{a,b,c},{p0(1),p0(2),p0(3)}));
+	err=(p1-p0)'*(p1-p0);
+	p0=p1;
+    y=double(subs(g,{a,b,c},{p0(1),p0(2),p0(3)}));
+	if (err<1e-16||y'*y<1e-16)
+		break
+	end
+end
 
 
-% for i=1:27
-%     if double(subs(E,{a,b,c},{X(1,i),X(2,i),X(3,i)}))<temp
-%         temp=double(subs(E,{a,b,c},{X(1,i),X(2,i),X(3,i)}))
-%         cand=[cand [X(1,i);X(2,i);X(3,i);temp]];
-%         double(subs(g,{a,b,c},{X(1,i),X(2,i),X(3,i)}))
-%     end
-% end
+for i=1:27
+    if double(subs(E,{a,b,c},{X(1,i),X(2,i),X(3,i)}))<temp
+        temp=double(subs(E,{a,b,c},{X(1,i),X(2,i),X(3,i)}))
+        cand=[cand [X(1,i);X(2,i);X(3,i);temp]];
+        double(subs(g,{a,b,c},{X(1,i),X(2,i),X(3,i)}))
+    end
+end
